b77c218bd09a78d963bb52c1168a31ec32f17136
Senyo Dedoo <mrdee@Senyos-MacBook-Air.local>
2024-05-16 11:42:31 +0000

    2: Task 2 and readme explanation

--- README.md
+++ README.md
@@ -4,6 +4,6 @@
 
 ## Tasks
 
-- Task 1 takes is a function that takes an array of numbers and returns the square of all its even numbers and triples all of its odd numbers.
-- 2 1
+- Task 1 is a function that takes an array of numbers and returns the square of all its even numbers and triples all of its odd numbers.
+- Task 2 is a function that takes an array of strings as the first argument and array of processed numbers from task 1 as the second argument. The strings are turned into uppercase letters when they correspond to an even number and turned to lowercase letters when they correspond to an odd number.
 - 3
--- arrayManipulation.js
+++ arrayManipulation.js
@@ -11,3 +11,27 @@ const processArray = (arrayOfNumbers) => {
 // const numbers = [1, 2, 3, 4, 5];
 // const processedNumbers = processArray(numbers);
 // console.log(processedNumbers);
+
+const formatArrayStrings = (arrayOfStrings, processedNumbers) => {
+  if (arrayOfStrings.length !== processedNumbers.length) {
+    throw new Error("The length of the two arrays must be equal");
+  }
+
+  let modifiedStrings = [];
+
+  for (let i = 0; i < arrayOfStrings.length; i++) {
+    let string = arrayOfStrings[i];
+    let number = processedNumbers[i];
+
+    if (number % 2 === 0) modifiedStrings.push(string.toUpperCase());
+    else modifiedStrings.push(string.toLowerCase());
+  }
+
+  return modifiedStrings;
+};
+
+// const strings = ["hello", "world", "JavaScript", "react", "Native"];
+// const numbers = [1, 2, 3, 4, 5];
+// const processedNumbers = processArray(numbers);
+// const modifiedStrings = formatArrayStrings(strings, processedNumbers);
+// console.log(modifiedStrings);
